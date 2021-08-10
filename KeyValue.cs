0b717468db7927caf941904975a347b33ce99168
syedfaizz <syedfaizz45@gmail.com>
2021-08-10 10:10:07 +0530

    [faisal] Add: uc1-ability-to-find-frequency-of-words

---
+++ KeyValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTableProblems
+{
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+}
